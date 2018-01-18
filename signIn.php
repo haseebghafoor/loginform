c8c9ca1d136d0297054c6006aeba7507bea5cacb
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-18 19:44:52 +0500

    add men animation and adjust it

--- signIn.php
+++ signIn.php
@@ -3,10 +3,11 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="C:\wamp64\www\test1"></link>
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
 </head>
 <body>
     <div class="container">
+        <img src="images/men1.png">
     	<form action="$_SERVER['PHP_SELF']" method="post">
     		<div class="forminput">
     			<input type="text" name="username">
