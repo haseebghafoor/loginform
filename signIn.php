e8b3fce4f677121306bc84cad4f7ba648d65b75a
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-18 15:34:22 +0500

    create username and password fild

---
+++ signIn.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="C:\wamp64\www\test1"></link>
+</head>
+<body>
+    <div class="container">
+    	<form action="$_SERVER['PHP_SELF']" method="post">
+    		<div class="forminput">
+    			<input type="text" name="username">
+    		</div>
+    		   <div class="forminput">
+    			  <input type="pssword" name="password">
+    		</div>
+    	</form>
+       </div>
+
+
+       hffh
+
+</body>
+</html>
