d548638c97aff7ff5a67656d39c3ea3fabffc6e6
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-18 21:14:32 +0500

    creating and designing signin and signup button

--- signIn.php
+++ signIn.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 	<link rel="stylesheet" type="text/css" href="style.css"></link>
 </head>
 <body>
@@ -10,11 +11,17 @@
         <img src="images/men1.png">
     	<form action="$_SERVER['PHP_SELF']" method="post">
     		<div class="forminput">
-    			<input type="text" name="username">
+    			<input type="text" name="username" placeholder="Enter UserName">
     		</div>
     		   <div class="forminput">
-    			  <input type="pssword" name="password">
+    			  <input type="password" name="password" placeholder="Enter Password">
     		</div>
+            <div>
+                <input type="submit" name="submit" value="SIGNIN" class="btnsignin">
+            </div>
+            <div>
+               <a href="#"> <input type="button" name="signup" value="SIGNUP" class="btnsignup"></a>
+            </div>
     	</form>
        </div>
 
