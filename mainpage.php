4dec4a24e2cb0c1700cf5b79321e83c280a09916
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-30 23:16:53 +0500

    create main page

---
+++ mainpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
+</head>
+<body>
+   <div>
+    <?php include 'header.php' ?>
+   </div>
+
+
+       
+
+</body>
+</html>
