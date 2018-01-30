a7465402ac373284d046f9a9b3cea5c72480687f
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-30 23:12:47 +0500

    create header for all pages

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+	<link rel="stylesheet" type="text/css" href="style3.css"></link>
+</head>
+<body>
+    <div class="header">
+        <div class="logout">
+        
+        <a href=""  ><h4>Log out </h4></a>
+  
+</div>
+        </div>
+<div class="meanue">
+    <ul>
+        <li>
+            <a href="" ><h4>Students </h4></a>
+             <li>
+            <a href="course" ><h4>Course </h4></a>
+        </li>
+         <li>
+            <a href="" ><h4>Degree</h4></a>
+        </li>
+        </li>
+    </ul>
+</div>
+
+       
+
+</body>
+</html>
